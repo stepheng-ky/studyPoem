220dbe723341c2e60fb105900f04537072b63c5a
gky <1142262478@qq.com>
2024-04-23 16:07:22 +0800

    1.0.0体验

--- pages/createPlan/createPlan.js
+++ pages/createPlan/createPlan.js
@@ -10,16 +10,68 @@ Page({
 
   selectPoem(e) {
     const index = e.currentTarget.dataset.index;
-    const poems = this.data.poems;
-    poems[index].checked = !poems[index].checked;
+    const myPoems = this.data.poems;
+    myPoems[index].checked = !myPoems[index].checked;
     this.setData({
-      poems: poems
+      poems: myPoems
     });
   },
-
-  generatePlan() {
-    const selectedPoems = this.data.poems.filter(poem => poem.checked);
-    // 生成学习计划的逻辑，可以将选中的诗词保存到数据库或进行其他操作
-    console.log('生成学习计划:', selectedPoems);
-  }
+  
+  // 勾选诗词，弹框输入计划名称，打印计划明细
+  generatePlan() { 
+    wx.showModal({ 
+      title: '输入计划名', 
+      content: '请输入计划名称', 
+      editable: true,// 可输入
+      success: (res) => { 
+        if (res.confirm) { 
+          const planName = res.content; 
+          const selectedPoems = this.data.poems.filter(poem => poem.checked);  
+          console.log('生成学习计划,名称:',planName ,',内容:', selectedPoems); 
+        }else{
+          console.log('取消生成学习计划'); 
+        }
+      } 
+    }); 
+  },
+  copyPlan() {
+    const templates = ['模板A', '模板B', '模板C'];
+    wx.showActionSheet({ 
+      itemList: templates, 
+      success(res) { 
+        if (!res.cancel) { 
+          const selectedTemplate = templates[res.tapIndex]; 
+          wx.showModal({ 
+            title: '输入计划名', 
+            content: '请输入计划名称', 
+            editable: true,// 可输入
+            success: (res) => { 
+              if (res.confirm) { 
+                const planName = res.content; 
+                console.log('复制学习计划：',selectedTemplate, '-->',planName ); 
+              }else{
+                console.log('取消复制学习计划'); 
+              }
+            } 
+          }); 
+        } else { 
+          console.log('取消复制学习计划'); 
+        } 
+      } 
+    });
+  },
+  seeCopyPlan() {
+    const templates = ['模板A', '模板B', '模板C'];
+    wx.showActionSheet({ 
+      itemList: templates, 
+      success(res) { 
+        if (!res.cancel) { 
+          const selectedTemplate = templates[res.tapIndex]; 
+          console.log('查看模板详情:', selectedTemplate); 
+        } else { 
+          console.log('取消查看模板详情'); 
+        } 
+      } 
+    });
+  },
 });
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,24 +1,29 @@
 Page({
+  // 新建学习计划
   goToCreatePlan() {
     wx.navigateTo({
       url: '/pages/createPlan/createPlan'
     });
   },
+  // 显示学习进度
   goToProgress() {
     wx.navigateTo({
       url: '/pages/progress/progress'
     });
   },
+  // 开始学习
   goToStartLearning() {
     wx.navigateTo({
       url: '/pages/startLearning/startLearning'
     });
   },
+  // 查看学习计划
   goToViewPlan() {
     wx.navigateTo({
       url: '/pages/viewPlan/viewPlan'
     });
   },
+  // 查看诗词库
   goToPoemLibrary() {
     wx.navigateTo({
       url: '/pages/poemLibrary/poemLibrary'
