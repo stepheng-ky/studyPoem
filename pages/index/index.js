2ae6934612c3727e49ac6d25034d08f440635c51
那个姓郭的、 <1142262478@qq.com>
2024-04-26 23:28:09 +0800

    首页用请求接口数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,31 +1,36 @@
 Page({
   data: {
-    poemList: [
-      {
-        title: '静夜思',
-        author: '李白·唐',
-        content: '举头望明月\n低头思故乡'
-      },
-      {
-        title: '登鹳雀楼',
-        author: '王之涣·唐',
-        content: '欲穷千里目\n更上一层楼'
-      }
-    ],
     currentPoemAuthor: '',
-    currentPoemContent0: '',
-    currentPoemContent1: '',
+    currentPoemzhailu0: '',
+    currentPoemzhailu1: '',
   },
   onLoad: function () {
-    // 随机poemList中的任一poem
-    const randomPoemIndex = Math.floor(Math.random() * this.data.poemList.length);
-    const randomPoem = this.data.poemList[randomPoemIndex];
-
-    this.setData({
-      // 作者 第一句 第二句
-      currentPoemAuthor: randomPoem.author,
-      currentPoemContent0: randomPoem.content.split('\n')[0],
-      currentPoemContent1: randomPoem.content.split('\n')[1]
-    });
+    // 发起网络请求获取随机诗词  
+    wx.request({  
+      url: 'https://guomuyi.com/studypoem/random_poem', // 接口地址  
+      method: 'GET', // 请求方法  
+      success: function (res) {  
+        // 请求成功后的回调函数  
+        if (res.data && res.statusCode === 200) {  
+          // 接口返回的数据结构是 {"author":"","content":"","id":"","title":"","yiwen":","zhailu":""}
+          const { author, zhailu } = res.data;  
+          const zhailuLines = zhailu.split('\\n'); // 注意这里使用双反斜杠来匹配接口返回数据中的换行符  
+  
+          // 设置数据到页面  
+          this.setData({  
+            currentPoemAuthor: author,  
+            currentPoemzhailu0: zhailuLines[0],  
+            currentPoemzhailu1: zhailuLines.length > 1 ? zhailuLines[1] : '' // 确保有第二句才设置  
+          });  
+        } else {  
+          // 处理请求失败的情况  
+          console.error('获取诗词失败', res);  
+        }  
+      },  
+      fail: function (error) {  
+        // 请求失败后的回调函数  
+        console.error('请求失败', error);  
+      }  
+    });  
   }
 });
