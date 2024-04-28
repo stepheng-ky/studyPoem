a44bcdd9b1b2bb9704e4f39f3d7304fea8f6e3f7
guokeyuan <guokeyuan@longfor.com>
2024-04-28 18:10:30 +0800

    请求openid

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // app.js
+// 引入配置文件  
+import { API_BASE_URL } from 'config.js'; 
+
 App({
   onLaunch() {
     // 展示本地存储能力
@@ -7,11 +10,38 @@ App({
     wx.setStorageSync('logs', logs)
 
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+    wx.login({  
+      success: function(res) {  
+        if (res.code) {  
+          console.log('res.code:',res.code);
+          // 发起网络请求  
+          const RANDOM_POEM_API = `${API_BASE_URL}/openid`; // 拼接完整的接口地址
+          wx.request({  
+            url: RANDOM_POEM_API, // Flask应用运行的地址  
+            method: 'POST',  
+            data: {  
+              code: res.code  
+            },  
+            success: function(response) {  
+              if (response.data.openid) {  
+                // 成功获取openid，进行后续操作  
+                console.log('res.code:',res.code);
+                console.log('response.data:',response.data);
+              } else {  
+                // 处理错误  
+                console.error('Error:', response.data.error);  
+              }  
+            },  
+            fail: function(error) {  
+              // 处理请求失败的情况  
+              console.error('Request failed:', error);  
+            }  
+          });  
+        } else {  
+          console.log('登录失败！' + res.errMsg);  
+        }  
+      }  
+    });
   },
   globalData: {
     userInfo: null
