e49ccd9f49c05b6e03721a725b9e76f1bad814cb
那个姓郭的、 <1142262478@qq.com>
2024-07-03 23:46:49 +0800

    播放功能

--- pages/poemDetail/poemDetail.js
+++ pages/poemDetail/poemDetail.js
@@ -12,9 +12,10 @@ Page({
       yiwen: '',
       zhailu:'' ,
       errorMessage:'',
-    }  
-  },  
-  
+      isPlaying: false, // 添加一个状态来追踪音频播放
+    },
+    innerAudioContext:'',  
+  }, 
   onLoad: function (options) {  
     const that = this;  
     // 显示加载动画
@@ -44,7 +45,6 @@ Page({
           // 隐藏加载动画
           wx.hideLoading();
         }  else {  
-          console.log('失败~~');  
           // 处理请求失败的情况  
           that.setData({
             errorMessage: '获取诗词详情失败，请重试...'
@@ -60,5 +60,49 @@ Page({
           wx.hideLoading(); 
       }  
     }); 
+  },  
+  playAudio: function() {
+    const that = this;
+    let innerAudioContext = '';
+    // 判断innerAudioContext是否已经初始化
+    if(that.data.innerAudioContext){
+      innerAudioContext = that.data.innerAudioContext;
+    } else{
+      innerAudioContext = wx.createInnerAudioContext({
+        useWebAudioImplement: true
+      }); 
+      that.setData({
+        innerAudioContext
+      });
+    };
+    if (!that.data.poem.isPlaying) { // 如果音频没有播放
+      const poemId = that.data.poem.id;
+      const audioUrl = `${API_BASE_URL}/play/${poemId}`;
+      innerAudioContext.src = audioUrl;
+      innerAudioContext.play();
+      that.setData({
+        'poem.isPlaying': true, // 设置为正在播放
+      });
+      // 监听音频结束事件，结束时重置播放状态，释放资源
+      innerAudioContext.onEnded(() => {
+        that.setData({
+          'poem.isPlaying': false
+        });
+        innerAudioContext.stop();
+      });
+    } else { // 如果音频正在播放，则暂停
+      innerAudioContext.pause();
+      that.setData({
+        'poem.isPlaying': false // 设置为未播放
+      });
+    }
+  }, 
+  onUnload: function() {
+    // 释放音频资源
+    const that = this;
+    if(that.data.innerAudioContext){
+      const innerAudioContext = that.data.innerAudioContext;
+      innerAudioContext.destroy();
+    }
   },
 });
