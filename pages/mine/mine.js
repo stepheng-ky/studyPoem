dd4c4c787e0965c02d9fe0c89e0a394c1536892c
那个姓郭的、 <1142262478@qq.com>
2024-07-16 00:33:35 +0800

    tts页面

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -3,4 +3,10 @@ Page({
   data: {
     
   },
+  goToTTS: function() {  
+    // 执行页面跳转
+    wx.navigateTo({  
+      url: '/pages/tts/tts'
+    })  
+  },
 })
---
+++ pages/tts/tts.js
@@ -0,0 +1,10 @@
+import { API_BASE_URL } from '../../config.js'; 
+
+Page({
+  data: {
+    url: `${API_BASE_URL}/tts_web`,
+  },
+  onLoad: function() {  
+    // console.log('tts...')
+  }
+})
